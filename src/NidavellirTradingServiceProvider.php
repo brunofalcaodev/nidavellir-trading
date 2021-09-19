5f3f4fb8e7f2d059d1f4c17b271ed8d5909491f0
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-09-19 19:24:25 +0200

    wip

---
+++ src/NidavellirTradingServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nidavellir\Trading;
+
+use Illuminate\Support\ServiceProvider;
+
+class NidavellirTradingServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
