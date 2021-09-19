621f709cf2d624c38e65450dea00671c12907cf0
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-09-19 20:42:40 +0200

    Create ApiValidator.php

---
+++ src/Validators/ApiValidator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Nidavellir\Trading\Validators;
+
+use Nidavellir\Abstracts\Contracts\Validatable;
+
+class ApiValidator implements Validatable
+{
+    protected $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function parse()
+    {
+        return str_replace(' ', '', $this->value);
+    }
+
+    /**
+     * Validates the "api" instruction.
+     * The hash code should exist in the database, should be active, and
+     * also should be part of an active user.
+     *
+     * @param  string $value
+     *
+     * @return bool
+     */
+    public static function validate(string $value)
+    {
+        $value = (new static($value))->parse();
+
+        return $value == 'bruno';
+    }
+}
